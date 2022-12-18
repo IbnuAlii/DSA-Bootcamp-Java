4701216a7ce8210b00fe19ed4c2af1ed545383b0
Mohamed Ali Nor <114952970+IbnuAlii@users.noreply.github.com>
2022-12-18 20:35:29 +0300

    OOP 2 | Packages, Static, Singleton Class, In-built Methods

---
+++ Lessons/OOP 2/singleton/NotSubClass.java
@@ -0,0 +1,12 @@
+package com.mohan.singleton;
+
+import com.mohan.access.A;
+
+public class NotSubClass{
+
+
+    public static void main(String[] args) {
+        NotSubClass obj = new NotSubClass();
+//        int n = obj.num;
+    }
+}
