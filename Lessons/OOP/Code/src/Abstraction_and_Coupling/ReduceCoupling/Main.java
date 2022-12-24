247b2b06ddb5476ce796809f75291e3e7d6cbd78
Mohamed Ali Nor <114952970+IbnuAlii@users.noreply.github.com>
2022-12-24 20:07:26 +0300

    Delete Lessons/OOP directory

--- Lessons/OOP/Code/src/Abstraction_and_Coupling/ReduceCoupling/Main.java
+++
@@ -1,8 +0,0 @@
-package Abstraction_and_Coupling.ReduceCoupling;
-
-public class Main {
-    public static void main(String[] args) {
-        var browser = new Browser();
-        browser.navigate("192.168.1.2");
-    }
-}
