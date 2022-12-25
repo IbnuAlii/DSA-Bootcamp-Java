5c6c5c00b06fece1101592e1f8a8ada2ab25bf01
Mohamed Ali Nor <114952970+IbnuAlii@users.noreply.github.com>
2022-12-25 22:01:56 +0300

    02 Refactoring Towards an Object-oriented Design

---
+++ Lessons/Ultimate Java - Part 2 (OOP)/Code/src/02 Refactoring Towards an Object-oriented Design/Moving_Away_from_Static_Members/MortgageReport.java
@@ -0,0 +1,31 @@
+package Moving_Away_from_Static_Members;
+
+import java.text.NumberFormat;
+
+public class MortgageReport {
+
+    private  MortgateCalculator calculator;
+
+    public MortgageReport(MortgateCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= calculator.getYears() * Main.MONTHS_IN_YEAR; month++) {
+            double balance = calculator.calculateBalance(month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+}
