6edd75aae6ab3b950f6f5b9b23158a2636de8dbf
Mohamed Ali Nor <114952970+IbnuAlii@users.noreply.github.com>
2023-10-02 22:05:04 +0300

    Update AreaOfCircle.java

--- Assigments/03-conditionals-loops.md/AreaOfCircle.java
+++ Assigments/03-conditionals-loops.md/AreaOfCircle.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-public class AreaOfCircle{
-    public static void main(String args[]){
-        double area, radius;
-        System.out.println("Required packages have been imported");
-        Scanner my_scanner= new Scanner(System.in);
-        System.out.println("A Scanner object has been defined ");
-        System.out.println("Enter the radius of the circle:");
-        radius= my_scanner.nextDouble();
-        area=(22*radius*radius)/7 ;
-        System.out.println("The Area of Circle is: " + area);
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter The radius");
+        double radius = input.nextDouble();
+
+        double Area = CalculateArea(radius);
+        System.out.println("The area of of the circle is: " + Area);
+    }
+    static double CalculateArea(double radius){
+        return Math.PI * radius * radius;
     }
 }
